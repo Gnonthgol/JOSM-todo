44a13a7fec2976a0c3f424b2c6ed43673abe90ed
Gnonthgol <gnonthgol@gmail.com>
2012-02-27 20:00:39 +0100

    Select element when opening popup menu

--- josm-todo/src/org/openstreetmap/josm/plugins/todo/TodoDialog.java
+++ josm-todo/src/org/openstreetmap/josm/plugins/todo/TodoDialog.java
@@ -8,7 +8,7 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
+
 import javax.swing.AbstractAction;
 import javax.swing.DefaultListSelectionModel;
 import javax.swing.JList;
@@ -26,20 +26,20 @@ import org.openstreetmap.josm.data.osm.event.SelectionEventManager;
 import org.openstreetmap.josm.gui.OsmPrimitivRenderer;
 import org.openstreetmap.josm.gui.SideButton;
 import org.openstreetmap.josm.gui.dialogs.ToggleDialog;
-import org.openstreetmap.josm.tools.ImageProvider;
-import org.openstreetmap.josm.tools.Shortcut;
 import org.openstreetmap.josm.gui.widgets.ListPopupMenu;
 import org.openstreetmap.josm.gui.widgets.PopupMenuLauncher;
+import org.openstreetmap.josm.tools.ImageProvider;
+import org.openstreetmap.josm.tools.Shortcut;
 
 public class TodoDialog extends ToggleDialog {
 
-	private static final long serialVersionUID = 3590739974800809827L;
-	private TodoListModel model;
-	private JList lstPrimitives;
-	private TodoPopup popupMenu;
-	private AddAction actAdd;
+    private static final long serialVersionUID = 3590739974800809827L;
+    private TodoListModel model;
+    private JList lstPrimitives;
+    private final TodoPopup popupMenu;
+    private AddAction actAdd;
 
-	/**
+    /**
      * Builds the content panel for this dialog
      */
     protected void buildContentPanel() {
@@ -56,160 +56,161 @@ public class TodoDialog extends ToggleDialog {
         final SideButton selectButton = new SideButton(actSelect = new SelectAction(model));
         lstPrimitives.getSelectionModel().addListSelectionListener(actSelect);
 
-		// the add button
+        // the add button
         final SideButton addButton = new SideButton(actAdd = new AddAction(model));
-        
+
         // the mark button
         MarkAction actMark;
         final SideButton markButton = new SideButton(actMark = new MarkAction(model));
         lstPrimitives.getSelectionModel().addListSelectionListener(actMark);
         Main.registerActionShortcut(actMark, Shortcut.registerShortcut("subwindow:todo:mark",
-        		tr("Mark element done"), KeyEvent.VK_TAB, Shortcut.DIRECT));
+                tr("Mark element done"), KeyEvent.VK_TAB, Shortcut.DIRECT));
 
         createLayout(lstPrimitives, true, Arrays.asList(new SideButton[] {
-            selectButton, addButton, markButton
+                selectButton, addButton, markButton
         }));
     }
 
-	
-	public TodoDialog() {
-		super(tr("Todo list"), "todo", tr("Open the todo list."),
-		        Shortcut.registerShortcut("subwindow:todo", tr("Toggle: {0}", tr("Todo list")),
-		                KeyEvent.VK_T, Shortcut.CTRL_SHIFT), 150);
-		buildContentPanel();
-		
-		lstPrimitives.addMouseListener(new DblClickHandler());
-		lstPrimitives.addMouseListener(new TodoPopupLauncher());
 
-		popupMenu = new TodoPopup(lstPrimitives);
-	}
+    public TodoDialog() {
+        super(tr("Todo list"), "todo", tr("Open the todo list."),
+                Shortcut.registerShortcut("subwindow:todo", tr("Toggle: {0}", tr("Todo list")),
+                        KeyEvent.VK_T, Shortcut.CTRL_SHIFT), 150);
+        buildContentPanel();
 
-	protected static void selectAndZoom(OsmPrimitive object) {
-		if (object == null) return;
+        lstPrimitives.addMouseListener(new DblClickHandler());
+        lstPrimitives.addMouseListener(new TodoPopupLauncher());
+
+        popupMenu = new TodoPopup(lstPrimitives);
+    }
+
+    protected static void selectAndZoom(OsmPrimitive object) {
+        if (object == null) return;
         if (Main.map == null || Main.map.mapView == null || Main.map.mapView.getEditLayer() == null) return;
         Main.map.mapView.getEditLayer().data.setSelected(object);
         AutoScaleAction.autoScale("selection");
-	}
-	
-	@Override
-	public void showNotify() {
-		SelectionEventManager.getInstance().addSelectionListener(actAdd, FireMode.IN_EDT_CONSOLIDATED);
-	}
-		        
-	private class SelectAction extends AbstractAction implements ListSelectionListener {
-	    private TodoListModel model;
+    }
 
-		public SelectAction(TodoListModel model) {
-			this.model = model;
-	        putValue(NAME, tr("Zoom"));
-	        putValue(SHORT_DESCRIPTION,  tr("Select and zoom to the selected item"));
-	        putValue(SMALL_ICON, ImageProvider.get("dialogs","zoom-best-fit"));
-	        updateEnabledState();
-	    }
+    @Override
+    public void showNotify() {
+        SelectionEventManager.getInstance().addSelectionListener(actAdd, FireMode.IN_EDT_CONSOLIDATED);
+    }
 
-	    public void actionPerformed(ActionEvent e) {
-	        selectAndZoom(model.getSelected());
-	    }
+    private class SelectAction extends AbstractAction implements ListSelectionListener {
+        private final TodoListModel model;
 
-	    public void updateEnabledState() {
-	        setEnabled(model.getSelected() != null);
-	    }
+        public SelectAction(TodoListModel model) {
+            this.model = model;
+            putValue(NAME, tr("Zoom"));
+            putValue(SHORT_DESCRIPTION,  tr("Select and zoom to the selected item"));
+            putValue(SMALL_ICON, ImageProvider.get("dialogs","zoom-best-fit"));
+            updateEnabledState();
+        }
 
-	    public void valueChanged(ListSelectionEvent e) {
-	        updateEnabledState();
-	    }
-	}
-	
-	private class AddAction extends AbstractAction implements SelectionChangedListener {
-	    private TodoListModel model;
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            selectAndZoom(model.getSelected());
+        }
 
-		public AddAction(TodoListModel model) {
-			this.model = model;
-	        putValue(NAME, tr("Add"));
-	        putValue(SHORT_DESCRIPTION,  tr("Add the selected items to the todo list."));
-	        putValue(SMALL_ICON, ImageProvider.get("dialogs","add"));
-	        updateEnabledState();
-	    }
+        public void updateEnabledState() {
+            setEnabled(model.getSelected() != null);
+        }
 
-	    public void actionPerformed(ActionEvent e) {
-	        if (Main.map == null || Main.map.mapView == null || Main.map.mapView.getEditLayer() == null) return;
-	        Collection<OsmPrimitive> sel = Main.map.mapView.getEditLayer().data.getSelected();
-	        model.addItems(sel);
-	    }
+        @Override
+        public void valueChanged(ListSelectionEvent e) {
+            updateEnabledState();
+        }
+    }
 
-	    public void updateEnabledState() {
-	    	if (Main.map == null || Main.map.mapView == null || Main.map.mapView.getEditLayer() == null) {
-	    		setEnabled(false);
-	    	} else {
-	    		setEnabled(!Main.map.mapView.getEditLayer().data.selectionEmpty());
-	    	}
-	    }
-	    
-		@Override
-		public void selectionChanged(Collection<? extends OsmPrimitive> arg) {
-			updateEnabledState();
-		}
-	}
-	
-	private class MarkAction extends AbstractAction implements ListSelectionListener {
+    private class AddAction extends AbstractAction implements SelectionChangedListener {
+        private final TodoListModel model;
 
-		TodoListModel model;
-		
-		public MarkAction(TodoListModel model) {
-			this.model = model;
-			putValue(NAME, tr("Mark"));
-	        putValue(SHORT_DESCRIPTION,  tr("Mark the selected item as done."));
-	        putValue(SMALL_ICON, ImageProvider.get("dialogs","check"));
-	        updateEnabledState();
-		}
+        public AddAction(TodoListModel model) {
+            this.model = model;
+            putValue(NAME, tr("Add"));
+            putValue(SHORT_DESCRIPTION,  tr("Add the selected items to the todo list."));
+            putValue(SMALL_ICON, ImageProvider.get("dialogs","add"));
+            updateEnabledState();
+        }
 
-		@Override
-		public void actionPerformed(ActionEvent arg0) {
-			model.removeSelected();
-			selectAndZoom(model.getSelected());
-		}
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (Main.map == null || Main.map.mapView == null || Main.map.mapView.getEditLayer() == null) return;
+            Collection<OsmPrimitive> sel = Main.map.mapView.getEditLayer().data.getSelected();
+            model.addItems(sel);
+        }
 
-		public void updateEnabledState() {
-	        setEnabled(model.getSelected() != null);
-	    }
-		
-		@Override
-		public void valueChanged(ListSelectionEvent arg0) {
-			updateEnabledState();
-		}
-		
-	}
-	
-	/**
-	 * Responds to double clicks on the list of selected objects
-	 */
-	class DblClickHandler extends MouseAdapter {
-		@Override
-		public void mouseClicked(MouseEvent e) {
-			if (e.getClickCount() < 2 || ! SwingUtilities.isLeftMouseButton(e)) return;
-			selectAndZoom(model.getSelected());
-		}
-	}
+        public void updateEnabledState() {
+            if (Main.map == null || Main.map.mapView == null || Main.map.mapView.getEditLayer() == null) {
+                setEnabled(false);
+            } else {
+                setEnabled(!Main.map.mapView.getEditLayer().data.selectionEmpty());
+            }
+        }
+
+        @Override
+        public void selectionChanged(Collection<? extends OsmPrimitive> arg) {
+            updateEnabledState();
+        }
+    }
+
+    private class MarkAction extends AbstractAction implements ListSelectionListener {
+
+        TodoListModel model;
+
+        public MarkAction(TodoListModel model) {
+            this.model = model;
+            putValue(NAME, tr("Mark"));
+            putValue(SHORT_DESCRIPTION,  tr("Mark the selected item as done."));
+            putValue(SMALL_ICON, ImageProvider.get("dialogs","check"));
+            updateEnabledState();
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent arg0) {
+            model.removeSelected();
+            selectAndZoom(model.getSelected());
+        }
+
+        public void updateEnabledState() {
+            setEnabled(model.getSelected() != null);
+        }
+
+        @Override
+        public void valueChanged(ListSelectionEvent arg0) {
+            updateEnabledState();
+        }
+
+    }
 
     /**
-      * The popup menu launcher.
-      */
+     * Responds to double clicks on the list of selected objects
+     */
+    class DblClickHandler extends MouseAdapter {
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            if (e.getClickCount() < 2 || ! SwingUtilities.isLeftMouseButton(e)) return;
+            selectAndZoom(model.getSelected());
+        }
+    }
+
+    /**
+     * The popup menu launcher.
+     */
     class TodoPopupLauncher extends PopupMenuLauncher {
         @Override
         public void launch(MouseEvent evt) {
-            //if(model.getSelected().isEmpty()) {
-            //int idx = lstPrimitives.locationToIndex(evt.getPoint());
-            //if(idx < 0)  return;
-            //model.setSelected(Collections.singleton((OsmPrimitive)model.getElementAt(idx)));
-            //}
+            int idx = lstPrimitives.locationToIndex(evt.getPoint());
+            if(idx < 0)  return;
+            model.setSelected((OsmPrimitive)model.getElementAt(idx));
 
             popupMenu.show(lstPrimitives, evt.getX(), evt.getY());
         }
     }
 
     /**
-      * A right-click popup context menu for setting options and performing other functions on the todo list items.
-      */
+     * A right-click popup context menu for setting options and performing other functions on the todo list items.
+     */
     class TodoPopup extends ListPopupMenu {
         public TodoPopup(JList list) {
             super(list);
--- josm-todo/src/org/openstreetmap/josm/plugins/todo/TodoListModel.java
+++ josm-todo/src/org/openstreetmap/josm/plugins/todo/TodoListModel.java
@@ -2,61 +2,64 @@ package org.openstreetmap.josm.plugins.todo;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
 import javax.swing.AbstractListModel;
 import javax.swing.DefaultListSelectionModel;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
 
 import org.openstreetmap.josm.data.osm.OsmPrimitive;
 
 public class TodoListModel extends AbstractListModel {
 
-	private final ArrayList<OsmPrimitive> todoList = new ArrayList<OsmPrimitive>();
-	private DefaultListSelectionModel selectionModel;
-	
-	public TodoListModel(DefaultListSelectionModel selectionModel) {
-		this.selectionModel = selectionModel;
-	}
+    private final ArrayList<OsmPrimitive> todoList = new ArrayList<OsmPrimitive>();
+    private final DefaultListSelectionModel selectionModel;
 
-	@Override
-	public Object getElementAt(int index) {
-		return todoList.get(index);
-	}
+    public TodoListModel(DefaultListSelectionModel selectionModel) {
+        this.selectionModel = selectionModel;
+    }
 
-	@Override
-	public int getSize() {
-		return todoList.size();
-	}
+    @Override
+    public Object getElementAt(int index) {
+        return todoList.get(index);
+    }
 
-	public OsmPrimitive getSelected() {
-		if (getSize() == 0 || selectionModel.isSelectionEmpty()) return null;
-		return todoList.get(selectionModel.getMinSelectionIndex());
-	}
+    @Override
+    public int getSize() {
+        return todoList.size();
+    }
 
-	public void addItems(Collection<OsmPrimitive> items) {
-	    int size = getSize();
-	    if (size == 0) {
-	        todoList.addAll(items);
-	        super.fireIntervalAdded(this, 0, getSize());
-	        selectionModel.setSelectionInterval(0, 0);
-	    } else {
-	        todoList.ensureCapacity(size + items.size());
-	        for (OsmPrimitive item: items) {
-	            if (!todoList.contains(item))
-	                todoList.add(item);
-	        }
-	        super.fireIntervalAdded(this, size, getSize());
-	    }
-	}
+    public OsmPrimitive getSelected() {
+        if (getSize() == 0 || selectionModel.isSelectionEmpty()) return null;
+        return todoList.get(selectionModel.getMinSelectionIndex());
+    }
 
-	public void removeSelected() {
-		int sel = selectionModel.getMaxSelectionIndex();
-		todoList.remove(sel);
-		super.fireIntervalRemoved(this, sel, sel);
-		if (sel == getSize()) sel = 0;
-		selectionModel.setSelectionInterval(sel, sel);
-	}
+    public void addItems(Collection<OsmPrimitive> items) {
+        int size = getSize();
+        if (size == 0) {
+            todoList.addAll(items);
+            super.fireIntervalAdded(this, 0, getSize());
+            selectionModel.setSelectionInterval(0, 0);
+        } else {
+            todoList.ensureCapacity(size + items.size());
+            for (OsmPrimitive item: items) {
+                if (!todoList.contains(item))
+                    todoList.add(item);
+            }
+            super.fireIntervalAdded(this, size, getSize());
+        }
+    }
+
+    public void removeSelected() {
+        int sel = selectionModel.getMaxSelectionIndex();
+        todoList.remove(sel);
+        super.fireIntervalRemoved(this, sel, sel);
+        if (sel == getSize()) sel = 0;
+        selectionModel.setSelectionInterval(sel, sel);
+    }
+
+    public void setSelected(OsmPrimitive element) {
+        int sel = todoList.indexOf(element);
+        if (sel == -1) return;
+        selectionModel.setSelectionInterval(sel, sel);
+    }
 
 }
