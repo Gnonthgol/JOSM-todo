236e3b4c4c973820daa5c1f18a6f0efdc21b2b52
Gnonthgol <gnonthgol@gmail.com>
2012-10-10 17:30:39 +0200

    Construct dialog only when needed because of side effects

--- josm-todo/src/org/openstreetmap/josm/plugins/todo/TodoPlugin.java
+++ josm-todo/src/org/openstreetmap/josm/plugins/todo/TodoPlugin.java
@@ -6,16 +6,18 @@ import org.openstreetmap.josm.plugins.PluginInformation;
 
 public class TodoPlugin extends Plugin {
 
-	private TodoDialog todoDialog;
+    private TodoDialog todoDialog = null;
 
-	public TodoPlugin(PluginInformation info) {
-		super(info);
-		todoDialog = new TodoDialog();
+    public TodoPlugin(PluginInformation info) {
+        super(info);
     }
 
     @Override
     public void mapFrameInitialized(MapFrame oldFrame, MapFrame newFrame) {
-        if(newFrame != null)
+        if(newFrame != null && newFrame.mapView != null) {
+            if (todoDialog == null)
+                todoDialog = new TodoDialog();
             newFrame.addToggleDialog(todoDialog);
+        }
     }
 }
